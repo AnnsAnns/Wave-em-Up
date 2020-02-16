b2ee8bcba8980728c3e959093a8c91ccbb14f99d
tomGER <tumger@outlook.de>
2020-02-16 15:32:39 +0100

    Add Audio-support to weapon (For later usage)

--- Scripts/Gun.gd
+++ Scripts/Gun.gd
@@ -4,8 +4,10 @@ func gun_functions():
 	
 	if Input.is_action_pressed("left_mouse"):
 		$SynthwaveParticle.emitting = true
+		$Music.stream_paused = false
 	else:
 		$SynthwaveParticle.emitting = false
+		$Music.stream_paused = true
 
 func _process(delta):
 	gun_functions()
