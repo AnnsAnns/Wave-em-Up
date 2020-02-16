3b6cc2c66a0346305e3229009a693b55a87e8db0
tomGER <tumger@outlook.de>
2020-02-16 20:05:02 +0100

    Add Enemy / Improve Gun Particle

--- Scripts/Levels/test_level.gd
+++ Scripts/Levels/test_level.gd
@@ -6,9 +6,7 @@ func _ready():
 	$Player/KinematicBody2D/Gun.hide()
 
 func _on_Area2D_body_entered(body):
-	print("b")
 	if body == $Player/KinematicBody2D and not PlayerPickedUpGun:
-		print("Pog")
 		$PickupGun.hide()
 		$Player/KinematicBody2D/Gun.show()
 		PlayerPickedUpGun = true
