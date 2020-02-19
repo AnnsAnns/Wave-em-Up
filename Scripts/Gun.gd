f3a381882069420c5e7fbded73a097a240115fe1
Vergu-0 <Vergu-0@users.noreply.github.com>
2020-02-20 01:59:09 +0300

    Added Animated Player and Fixed Particle Curving
    Character is in the Entities folder.
    Randomized Level is the only level that uses the animated character.

--- Scripts/Gun.gd
+++ Scripts/Gun.gd
@@ -3,6 +3,9 @@ extends KinematicBody2D
 signal IsFiring
 signal IsNotFiring
 
+var lastRot = float()
+var lastPos = Vector2()
+
 func gun_functions():
 	if Input.is_action_pressed("left_mouse"):
 		$SynthwaveParticle.emitting = true
@@ -13,3 +16,12 @@ func gun_functions():
 
 func _process(delta):
 	gun_functions()
+
+func _physics_process(delta):
+	if $"SmoothRay" == null:	return
+	
+	var dif = lastRot-rotation
+	$SmoothRay.look_at(lastPos)
+	$SmoothRay.rotation = lerp_angle($SmoothRay.rotation, 0, delta * 4)
+	lastPos = $SmoothRay.global_position + ($SmoothRay.get_global_transform().x * 60 * (abs(dif) + 1))
+	lastRot = rotation
