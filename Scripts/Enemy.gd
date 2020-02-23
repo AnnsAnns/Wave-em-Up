817d256afaa34b4930d8cff5092d62ed5e95686a
Vergu-0 <Vergu-0@users.noreply.github.com>
2020-02-23 06:23:01 +0300

    Added Desert Tileset and Desert Map Generation, Also set the level to Level_Desert in the Main Menu
    You can play the desert level by pressing <PLAY> from the main menu

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,22 +1,60 @@
 extends KinematicBody2D
 
+enum AI_STATE {IDLE, CHASE, ATTACK}
+var state = AI_STATE.IDLE
+
 var path : = PoolVector2Array()
 var speed = 100
+export (int) var maxHealth = 100
+var health = maxHealth
 
+export (float) var tileSize = 1
 onready var player = $"../Player/KinematicBody2D"
 onready var nav = $"../Navigation"
-onready var line = $"../Line2D"
+
+var t = 0.1
+var hit = false
+var distance_to_walk
+
+
+func _ready():
+	health = maxHealth
+	$Label.text = "HP: " + String(health)
+	t = 0.1
+	
+	state = AI_STATE.CHASE
+
+func _process(delta):
+	t -= delta
+	
+	if hit == true:	lose_health()
 
 func _physics_process(delta):
-	path = nav.get_simple_path(position, player.position)
-	line.points = path
+	path = nav.get_simple_path(position, player.global_position)
+	distance_to_walk = speed * delta
 	
-	var distance_to_walk = speed * delta
-	
+	match state:
+		AI_STATE.IDLE:	pass
+		AI_STATE.CHASE:	chase()
+		AI_STATE.ATTACK:	pass
+
+func chase():
 	if path.size() > 0:
 		for i in range(path.size()):
 			if position.distance_to(path[i]) > distance_to_walk:
 				look_at(path[i])
-				var dir = path[i] - position
+				var dir = (path[i]) - position
 				dir = dir.normalized()
+# warning-ignore:return_value_discarded
 				move_and_slide(dir * speed)
+
+func lose_health():
+	if t > 0:	return
+	if hit == false:	return
+	
+	health = clamp(health - 1, 0, maxHealth)
+	$Label.text = "HP: " + String(health)
+	t = 0.1
+	hit = false
+	if health <= 0:
+		queue_free()
