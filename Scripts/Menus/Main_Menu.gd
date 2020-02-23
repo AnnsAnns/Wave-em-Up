0363bab056a6beadd187fb71b1048924efbd2b68
tomGER <tumger@outlook.de>
2020-02-23 23:26:22 +0100

    [+] Main Menu, Music, Credits and other stuff

--- Scripts/Menus/Main_Menu.gd
+++ Scripts/Menus/Main_Menu.gd
@@ -1,14 +1,16 @@
 extends Control
 
-func _on_Play_button_up():
-	$"/root/Singleton".scene_goto("res://Scenes/Level/Level_Desert.tscn")
+var x = false
 
-func _on_Options_button_up():
-	$"/root/Singleton".scene_goto("res://Scenes/Main Menu/Options.tscn")
+func _process(delta):
+	if Input.is_action_pressed("pickup"):
+		$"/root/Singleton".scene_goto("res://Scenes/Level/Level_Desert.tscn")
 
-func _on_Credits_button_up():
-	pass # Replace with function body.
 
-func _on_Quit_button_up():
-	get_tree().quit()
-
+func _on_Flash_timeout():
+	if not x:
+		$LogoStart.hide()
+		x = true
+	else:
+		$LogoStart.show()
+		x = false
