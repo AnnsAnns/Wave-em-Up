a46268ea56f5e4cb6e723117671aef17e7f560ec
tomGER <tumger@outlook.de>
2020-02-24 00:49:09 +0100

    Add unfinished 80s attack and the rest

--- Scripts/WillowBlade_Boss.gd
+++ Scripts/WillowBlade_Boss.gd
@@ -1,7 +1,27 @@
-extends StaticBody2D
+extends KinematicBody2D
 
 var HP = 500
+var random_idk_im_tired = 0
 
 func lose_health():
-	clamp(HP - 1, 0, HP)
-	print(HP)
+	# Needs Fix
+	HP = clamp(HP - 1, 0, HP)
+
+func _on_WillowTimer_timeout():
+	if random_idk_im_tired == 0:
+		$Willowblade.animation = "Angry"
+	elif random_idk_im_tired == 1:
+		$Willowblade.animation = "Flex"
+	elif random_idk_im_tired == 2:
+		$Willowblade.animation = "default"
+	elif random_idk_im_tired == 3:
+		$Willowblade.animation = "looking_at_player"
+	
+	if random_idk_im_tired == 3:
+		random_idk_im_tired = 0
+	
+	random_idk_im_tired += 1
+
+
+func _on_AttackTimer_timeout():
+	pass # This would regulate which attack he chooses
